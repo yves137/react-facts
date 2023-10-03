f1d12cf00edb0897cee049faaae9f9a993a19485
yves137 <oneg2981@gmail.com>
2023-10-03 13:14:52 +0200

    update project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Navbar } from "./Navbar";
+import { Main } from "./Main";
+function App() {
+  return (
+    <div className="">
+      <Navbar/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
